fc6f15a7789df73fa100a5f52e405e00aaa5ad4a
maxrswan13 <maxrswan13@gmail.com>
2026-04-06 18:01:24 +0000

    Turtle 3.0

--- lessons/10_Turtles/30_Turtle_Tricks/30_Turtle_Tricks.py
+++ lessons/10_Turtles/30_Turtle_Tricks/30_Turtle_Tricks.py
@@ -14,7 +14,65 @@ import turtle                           # Tell Python we want to work with the t
 turtle.setup(600,600,0,0)               # Set the size of the window
 tina = turtle.Turtle()                  # Create a turtle named tina
 
-# Use tina.circle() to draw a circle, and tina.goto() to move tina to a new location
+import turtle                           # Tell Python we want to work with the turtle
+turtle.setup(600,600,0,0)               # Set the size of the window
+tina = turtle.Turtle()                  # Create a turtle named tina
+
+tina.shape('turtle')                    # Set the shape of the turtle to a turtle
+tina.speed(1.75)                           # Make the turtle move as fast, but not too fast.
+
+##
+## Move Tina to the Starting Position
+#
+
+tina.penup()                            # Lift the pen up so we can move tina without drawing
+tina.goto(-10, 10)                    # Move tina to the starting position
+tina.pendown()                          # Put the pen down so we can draw
+
+##
+## Draw a Square
+##
+
+tina.penup()     
+tina.goto(-10, 10)
+tina.pendown()     
+
+tina.pendown()
+tina.color('green')                       # Set the color of tina to red
+tina.begin_fill()
+tina.circle(50)
+tina.end_fill()
+##
+## Draw a Circle
+##
+
+tina.penup()     
+tina.goto(-10, -150)
+tina.pendown()     
+
+tina.pendown()
+tina.color('blue')                       # Set the color of tina to red
+tina.begin_fill()
+tina.circle(50)
+tina.end_fill()
+
+##
+## Write a Message
+##
+
+tina.penup()                            # Lift the pen up so we can move tina without drawing
+tina.goto(40, 40)
+tina.forward(20)                        # Move tina forward by 20
+tina.left(90)                           # Turn tina left by 90 degrees
+tina.forward(20)                        # Move tina forward by 20
+tina.write("I am turtle")         # Write the message "Why, hello there!"
+tina.backward(20)                       # Move tina backward by 20
+
+turtle.exitonclick()                    # Close the window when we click on it  
+
+# You're on your way, soon you'll be writing your own programs!
+# But first, let's save your progress. Continue with 
+# the next file, 03_Check_In_Your_Code.ipynb# Use tina.circle() to draw a circle, and tina.goto() to move tina to a new location
 # Use tina.begin_fill(), tina.end_fill(), and tina.fillcolor() to fill in the shapes
 
 ... # Your code here
