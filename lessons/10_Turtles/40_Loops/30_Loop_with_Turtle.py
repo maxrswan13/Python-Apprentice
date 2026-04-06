3badabca68357d437b8a1a9dbff8a2b909334dfc
maxrswan13 <maxrswan13@gmail.com>
2026-04-06 18:11:13 +0000

    loop 2.0

--- lessons/10_Turtles/40_Loops/30_Loop_with_Turtle.py
+++ lessons/10_Turtles/40_Loops/30_Loop_with_Turtle.py
@@ -7,3 +7,14 @@ write a new program that uses a loop to draw a pentagon.
 """
 
 ... # Your code here
+import turtle                           # Tell Python we want to work with the turtle
+turtle.setup(600,600,0,0)               # Set the size of the window
+
+tina = turtle.Turtle()                  # Create a turtle named tina
+
+tina.shape('turtle')                    # Set the shape of the turtle to a turtle
+tina.speed(1.75)                           # Make the turtle move as fast, but not too fast. 
+
+for i in range(5):
+    tina.forward (180)
+    tina.left (72)
