24d5959d85be98a75cfcd4ed8f57d48ad44efaf4
maxrswan13 <maxrswan13@gmail.com>
2026-04-13 18:20:21 +0000

    ashdf

--- lessons/10_Turtles/70_Projects/20_Tash_Me.py
+++ lessons/10_Turtles/70_Projects/20_Tash_Me.py
@@ -10,4 +10,40 @@ Hint: See 08a_More Turtle Programs, section 'Change the Background Image' and
 'Change the Turtle Shape'
 """
 
-... # Your code here
+#... # Your code here
+
+
+import turtle
+
+def set_background_image(window, image_name):
+    """Set the background image of the turtle window to the image with the given name."""
+
+    from pathlib import Path
+    from PIL import Image
+
+    image_dir = Path(__file__).parent.parent / "images"
+    image_path = str(image_dir / image_name)
+
+    image = Image.open(image_path)
+    
+    window.setup(image.width, image.height, startx=-10, starty=10)
+    window.bgpic(image_path)
+
+# Set up the screen
+import turtle                           # Tell Python we want to work with the turtle
+turtle.setup(600, 600,0,0)     # Set the size of the window
+
+tina = turtle.Turtle()                  # Create a turtle named tina
+
+screen = turtle.Screen()                # Get the screen that tina is on
+set_background_image(screen, "sanic_64.png") # Set the background image of the screen
+
+turtle.exitonclick()
+
+
+
+
+
+
+
+
